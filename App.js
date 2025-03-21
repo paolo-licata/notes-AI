53372971d72bad061c198adc9b7ceed4876a5699
paolo-licata <paolo_licata@outlook.com>
2025-03-21 16:25:07 +0100

    Set up basic App navigation and components for the project

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, Text } from "react-native";
-import * as Speech from "expo-speech";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { collection, addDoc } from "firebase/firestore";
-import { auth } from "./firebase";
-import { db } from "./firebase";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./LoginScreen";
+import NotesScreen from "./NotesScreen";
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [ note, setNote ] = useState("");
-  const { summary, setSummary} = useState("");
-
-  // Sign-in with Google
-  const signInWithGoogle = async() => {
-    const provider = new GoogleAuthProvider();
-  
-    try {
-      const result = await signInWithPopup(auth, provider);
-      console.log("User info:", result.user);
-    } catch (error) {
-      console.log("Google sign-in error:", error)
-    }
-  }
-
-  // Convert speech to text using expo speech
-  const recordSpeech = () => {
-    Speech.speak("Satrt speaking...")
-  }
-  
-  // Saves the note to Firebase adding the item to "notes" collection
-  const saveNote = async () => {
-      await addDoc(collection(db, "notes"), {
-        text: noteText,
-        summary: summaryText,
-        timestamp: new Date()
-      });
-      setNote("");
-      setSummary("");
-  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Notes" component={NotesScreen} options={{ title: "My Notes" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
+
+export default App;
